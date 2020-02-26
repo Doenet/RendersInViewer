8e7d4e9cb58c9464bff474111162e07560415f88
Kevin Charles <char0042@umn.edu>
2020-02-26 15:28:14 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+  }
+
+      
+
+  render() {
+    return (
+      <div>
+        Hi there
+      </div>
+    );
+  }  
+}
+
+export default App;
